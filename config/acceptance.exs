2370df0bc860d0cda9f19e43119d2ac88fa95035
Josef Eissing <josef.eissing@outlook.com>
2021-06-17 12:31:59 +0200

    add docu

--- config/acceptance.exs
+++ config/acceptance.exs
@@ -1,4 +1,5 @@
 # Dok https://spin.atomicobject.com/2018/10/22/elixir-test-multiple-environments/
+# https://elixirforum.com/t/how-do-you-setup-up-multiple-test-environments-in-your-projects/32725/9
 use Mix.Config
 
 import_config "test.exs"
